847328298fc3f129a01b627e3dbab527f8597f09
vincentiusvin <vincentius_vin@yahoo.com>
2023-09-08 16:15:31 +0700

    Migrated to compose, found out about docker DNS Bruh moment

--- kill.sh
+++
@@ -1,5 +0,0 @@
-#!
-sudo docker stop react
-sudo docker stop flask
-sudo docker rm react
-sudo docker rm flask
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!
-source ./kill.sh
-sudo docker run --name "react" --ip 172.17.0.2 -dp 0.0.0.0:80:80 photos-react
-sudo docker run --name "flask" --ip 172.17.0.3 -v ./flask/images:/mnt -dp 127.0.0.1:8000:8000 photos-flask
